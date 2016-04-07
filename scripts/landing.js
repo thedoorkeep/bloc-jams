dd955681e86ebc7ce09b26408553947977c66800
Tyler Schmidt <thedoorkeep@gmail.com>
2016-04-06 19:12:39 -0500

    checkpoint 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,30 @@
- var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+      
+    var pointsArray = document.getElementsByClassName('point');
      
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+     var animatePoints = function(points) {
+         
+         var revealPoint = function(index) {
+             points[index].style.opacity = 1;
+             points[index].style.transform = "scaleX(1) translateY(0)";
+             points[index].style.msTransform = "scaleX(1) translateY(0)";
+             points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+         };
+
+         for (var i =0; i < points.length; i++) {
+            revealPoint(i);
+         }
+     
      };
 
-     for (var i =0; i < points.length; i++) {
-        revealPoint(i);
+      window.onload = function() {
+        var sellingPoints = document.getElementsByClassName('selling-points')[0];
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+         window.addEventListener('scroll', function(event) {
+             if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+                 animatePoints(pointsArray);   
+             }
+         });
      }
- 
- };
