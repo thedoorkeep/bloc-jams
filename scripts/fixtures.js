5921aaf3a824af3a4532348b9b48da23cc538cd0
Tyler Schmidt <thedoorkeep@gmail.com>
2016-05-09 19:07:16 -0500

    32 progress

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+ var albumDescendents = {
+     title: 'Milo Goes to College',
+     artist: 'The Descendents',
+     label: 'New Alliance Records',
+     year: '1982',
+     albumArtUrl: 'http://www.amoeba.com/admin/uploads/albums/covers/other//Descendents_MiloGoesToCollege.jpg',
+     songs: [
+         { title: 'Myage', duration: '2:01' },
+         { title: 'I Wanna Be a Bear', duration: '0:44' },
+         { title: 'Im Not a Loser', duration: '1:29'},
+         { title: 'Parents', duration: '1:40' },
+         { title: 'Tonyage', duration: '0:58'}
+     ]
+ };
